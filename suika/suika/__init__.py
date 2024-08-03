e00826c0412476b9c8a7c6754d4161fb6e81a412
zdworkin <zachary.dworkin@gmail.com>
2024-08-02 18:00:32 -0700

    Switch to using pillow instead of DXCam
    Signed-off-by: zdworkin <zachary.dworkin@gmail.com>

--- suika/suika/__init__.py
+++ suika/suika/__init__.py
@@ -1,5 +1,11 @@
 # Description
 
+from suika.core.watcher.screen import WindowCapture
+
 # pylint: disable-next=missing-function-docstring
 def cli_entry(args=None) -> int:
+    wc = WindowCapture()
+    wc.take_screenshot()
+    wc.save_screenshot("screenshot.png")
+    wc.close_screenshot()
     return 0
--- suika/suika/core/watcher/screen.py
+++ suika/suika/core/watcher/screen.py
@@ -1,30 +1,20 @@
-import win32gui
-import dxcam
+from PIL import ImageGrab
 
 class WindowCapture:
 
     def __init__(self):
-        self.hwnd = self.get_hwnd()
-        window_rect = list(win32gui.GetWindowRect(self.hwnd))
-        # Adjust based on application
-        window_rect[0] += 8
-        window_rect[1] += 96
-        window_rect[2] -= 8
-        window_rect[3] -= 73
-        self.cam = dxcam.create(region=window_rect)
-
-    def get_hwnd(self):
-        windows_list = []
-
-        def get_window_info(hwnd, result):
-            win_text = win32gui.GetWindowText(hwnd)
-            windows_list.append((hwnd, win_text))
-
-        win32gui.EnumWindows(get_window_info, None)
-        for (hwnd, win_text) in windows_list:
-            if "Ryujinx" in win_text:
-                return hwnd
-        return 0
+        self.screenshot = None
+        pass
 
     def take_screenshot(self):
-        return self.cam.grab()
+        self.screenshot = ImageGrab.grab()
+
+    def save_screenshot(self, path):
+        self.screenshot.save("screenshot.png")
+
+    def close_screenshot(self):
+        self.screenshot.close()
+
+    # region
+    # Capture a specific region (left, top, right, bottom)
+    # screenshot = ImageGrab.grab(bbox=(100, 100, 500, 500))
